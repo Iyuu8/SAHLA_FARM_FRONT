a8e2e318b9153eb3a396e0e420bb344d68644e9a
Iyuu8 <anesbenaziza19@gmail.com>
2026-03-22 12:45:11 +0100

    login page

--- src/layout.jsx
+++ src/layout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router'
 
 export default function Layout() {
   return (
-    <div className=''>
+    <div className='font-newblack'>
         <header>header</header>
         <div>sidebar</div>
         <main>
