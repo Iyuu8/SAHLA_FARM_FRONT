046c036e26ef65a3e0dd42c633c0df31b525b6d2
Iyuu8 <anesbenaziza19@gmail.com>
2026-03-22 09:27:49 +0100

    layout updated

---
+++ src/layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+export default function Layout() {
+  return (
+    <div className=''>
+        <header>header</header>
+        <div>sidebar</div>
+        <main>
+            <Outlet/>
+        </main>
+    </div>
+  )
+}
